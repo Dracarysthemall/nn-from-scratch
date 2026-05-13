b73ff88ae6a72caa14f46f1a0e246dbfa056c837
Dracarysthemall <bellouch30@gmail.com>
2026-05-13 16:32:59 +0100

    initial commit

---
+++ dense.py
@@ -0,0 +1,24 @@
+from layer import Layer
+import numpy as np
+
+
+
+class Dense(Layer):
+    def __init__(self, input_size, output_size):
+        self.weights = np.random.randn(output_size, input_size)
+        self.biases = np.random.randn(output_size, 1)
+        
+    def forward(self, input):
+        self.input = input
+        self.output = np.dot(self.weights, input) + self.biases
+        return self.output
+    
+    def backward(self, output_gradient, learning_rate):
+        weights_gradient = np.dot(output_gradient, self.input.T)
+        input_gradient = np.dot(self.weights.T, output_gradient)
+        self.weights -= learning_rate * weights_gradient
+        self.bias -= learning_rate * output_gradient
+        return input_gradient
+    
+    
+    
